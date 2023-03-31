c416478bf1dbddb869fccee989502b5d7394ff21
razibhosen24 <90138277+razibhosen24@users.noreply.github.com>
2023-03-31 11:16:06 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+
+// tooltip start
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+// tooltip end
+
+
+// toast1 start
+const toastTrigger = document.getElementById('liveToastBtn2')
+const toastLiveExample = document.getElementById('liveToast2')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+// toast1 end
+
+// alert start
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('Don&#39;t be worried, Allah makes the impossible possible.', 'success')
+  })
+}
+// alert end
+
